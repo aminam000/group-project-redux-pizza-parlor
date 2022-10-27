cdfcc4caf85ffc27050b8c57b7b23a4fbdf3378c
Astrid Pulse <astridcpulse@gmail.com>
2022-10-27 16:01:32 -0500

    adit pizzaSelect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -29,11 +29,15 @@ function App() {
         });
 }), [];
 
+
+
   return (
     <Router>
       <div className='App'>
         <header className='App-header'>
           <h1 className='App-title'>Prime Pizza</h1>
+
+
         </header>
     
         <img src='images/pizza_photo.png' />
