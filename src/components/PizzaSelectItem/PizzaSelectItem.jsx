2a7600be40814dbcb7ff5f8672044e5063b39f58
Astrid Pulse <astridcpulse@gmail.com>
2022-10-27 16:27:21 -0500

    create PizzaSelectItem component which generates each individual pizza menu item

---
+++ src/components/PizzaSelectItem/PizzaSelectItem.jsx
@@ -0,0 +1,48 @@
+import { useDispatch } from 'react-redux';
+import { useState} from 'react';
+
+function PizzaSelectItem({pizza}, {index}){
+    const [active, setActive] = useState(true);
+
+    const dispatch = useDispatch();
+
+
+    const addPizza = () => {
+        console.log('add pizzza');
+        setActive((previousStatus) => {
+            return !previousStatus;
+          });
+    }
+    const removePizza = () => {
+        console.log('remove pizza');
+        setActive((previousStatus) => {
+            return !previousStatus;
+          });
+    }
+    return(
+        <>
+            <div key={index}> 
+                <img src={pizza.image_path} />
+                    <p>{pizza.name}</p>
+                    <p>{pizza.description}</p>
+                    <p>{pizza.price}</p>
+                    
+                    {active ? (
+                        <button className="active"
+                            onClick={() => addPizza()}> 
+                            ADD
+                        </button>
+                    ) : (
+                        <button className="inactive"
+                            onClick={() => removePizza()}> 
+                            REMOVE
+                        </button>
+                    )}
+                    
+                </div>
+        
+        </>
+    );
+}
+
+export default PizzaSelectItem;
